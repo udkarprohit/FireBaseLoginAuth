60c6d26505bc810b967b63588cda2cef1a8040c4
udkarprohit <udkarprohit@gmail.com>
2025-01-15 15:43:05 +0530

    Initial Commit

---
+++ FireBaseLoginAuth/FireBaseLoginAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBaseLoginAuthApp.swift
+//  FireBaseLoginAuth
+//
+//  Created by Udkar Bittu on 15/01/25.
+//
+
+import SwiftUI
+
+@main
+struct FireBaseLoginAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
